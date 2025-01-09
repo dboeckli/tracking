09791a622c9f94a77c789a7d35e018abfff92439
adm-boeckli <dboeckli@gmail.com>
2025-01-09 10:16:09 +0100

    add embedded kafka test

--- src/test/java/dev/lydtech/tracking/TrackingApplicationTest.java
+++ src/test/java/dev/lydtech/tracking/TrackingApplicationTest.java
@@ -2,8 +2,10 @@ package dev.lydtech.tracking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("docker")
 class TrackingApplicationTest {
 
     @Test
