46ec229be80ee605612f6d388d736b25bbdbf2cd
Dominique Boeckli <dboeckli@gmail.com>
2025-01-12 16:53:40 +0100

    add handler

--- src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
+++ src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
@@ -4,25 +4,27 @@ import dev.lydtech.tracking.message.DispatchPreparing;
 import dev.lydtech.tracking.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@KafkaListener(
+    id = "dispatchTrackingConsumerClient",
+    topics = DispatchTrackingHandler.DISPATCH_TRACKING_TOPIC,
+    groupId = DispatchTrackingHandler.DISPATCH_TRACKING_TOPIC_GROUP,
+    containerFactory = "kafkaListenerContainerFactory"
+)
 public class DispatchTrackingHandler {
     
     public static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
     public static final String DISPATCH_TRACKING_TOPIC_GROUP = "tracking.dispatch.tracking";
 
     private final TrackingService trackingService;
-
-    @KafkaListener(
-        id = "dispatchTrackingConsumerClient",
-        topics = DISPATCH_TRACKING_TOPIC,
-        groupId = DISPATCH_TRACKING_TOPIC_GROUP,
-        containerFactory = "kafkaListenerContainerFactory"
-    )
+    
+    @KafkaHandler
     public void listen(DispatchPreparing dispatchPreparing) {
         log.info("Received dispatch tracking message: {}", dispatchPreparing);
         try {
