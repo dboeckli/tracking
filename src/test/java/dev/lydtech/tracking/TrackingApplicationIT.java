a8309d435b334ca4b4cf95354d718f6905edc402
Dominique Boeckli <dboeckli@gmail.com>
2025-01-12 16:47:19 +0100

    refactor project

--- src/test/java/dev/lydtech/tracking/TrackingApplicationIT.java
+++ src/test/java/dev/lydtech/tracking/TrackingApplicationIT.java
@@ -1,17 +1,20 @@
 package dev.lydtech.tracking;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @DirtiesContext
+@Slf4j
 @ActiveProfiles("docker")
 class TrackingApplicationIT {
 
     @Test
     void contextLoads() {
+        log.info("Testing Spring 6 Application...");
     }
 
 }
