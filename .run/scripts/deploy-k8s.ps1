d71676102a8bd18bd22e24d12f0f2c25cf6e2633
Dominique Boeckli <dboeckli@gmail.com>
2025-07-17 11:04:15 +0200

    fix renovate 2

--- .run/scripts/deploy-k8s.ps1
+++ .run/scripts/deploy-k8s.ps1
@@ -4,4 +4,4 @@ $file = Get-ChildItem -Filter dispatch-v*.tgz | Select-Object -First 1
 tar -xvf $file.Name
 
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
-helm upgrade --install $APPLICATION_NAME ./$APPLICATION_NAME -f "./$APPLICATION_NAME/dependencies-values.yaml" --namespace dispatch --create-namespace --wait --timeout 8m --debug --render-subchart-notes
+helm upgrade --install $APPLICATION_NAME ./$APPLICATION_NAME --namespace dispatch --create-namespace --wait --timeout 8m --debug --render-subchart-notes
--- README.md
+++ README.md
@@ -90,7 +90,7 @@ tar -xvf $file.Name
 install
 ```powershell
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
-helm upgrade --install $APPLICATION_NAME ./$APPLICATION_NAME -f "./$APPLICATION_NAME/dependencies-values.yaml" --namespace tracking --create-namespace --wait --timeout 8m --debug --render-subchart-notes
+helm upgrade --install $APPLICATION_NAME ./$APPLICATION_NAME --namespace tracking --create-namespace --wait --timeout 8m --debug --render-subchart-notes
 ```
 
 show logs
