ecd03f3e0cbf056bf0712617671aee0611b3f728
Dominique Boeckli <dboeckli@gmail.com>
2025-07-23 00:20:50 +0200

    fixup! runner

--- .run/scripts/uninstall-k8s.ps1
+++ .run/scripts/uninstall-k8s.ps1
@@ -1,5 +1,6 @@
 cd target/helm/repo
 
+$file = Get-ChildItem -Filter spring-6-icecold-micro-service-v*.tgz | Select-Object -First 1
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
 
 helm uninstall $APPLICATION_NAME --namespace dispatch
