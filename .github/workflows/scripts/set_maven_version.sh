0e97f31a37600673b382b23ce06f404d1b10dbee
Dominique Boeckli <dboeckli@gmail.com>
2025-07-22 23:31:53 +0200

    fixup! improve helm and versioning

--- .github/workflows/scripts/set_maven_version.sh
+++ .github/workflows/scripts/set_maven_version.sh
@@ -49,7 +49,13 @@ echo "### Prefix is: $prefix"
 
 # Build new version
 if [[ "$branch" != "master" ]] && [[ "$branch" != "main" ]]; then
-  NEW_MAVEN_VERSION=${prefix}_${branch}-${suffix}
+  # Calculate available length for branch name
+  available_length=$((63 - ${#prefix} - ${#suffix} - 2))  # 2 for the separators
+
+  # Truncate branch name if necessary
+  truncated_branch=$(echo "$branch" | cut -c1-$available_length)
+
+  NEW_MAVEN_VERSION="${prefix}_${truncated_branch}-${suffix}"
 else
   NEW_MAVEN_VERSION=$MVN_VERSION
 fi
